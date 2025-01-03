aa629e8de40b75c298c1ad973c0cd3e7cf1ce4f0
Max-Dil <kleska16@gmail.com>
2025-01-03 13:04:39 +0300

    update
    Bug fix
    Optimize

--- Solar 2d TS game/main.lua
+++ Solar 2d TS game/main.lua
@@ -1,6 +1,6 @@
 local ____lualib = require("lualib_bundle")
 local __TS__SourceMapTraceBack = ____lualib.__TS__SourceMapTraceBack
-__TS__SourceMapTraceBack(debug.getinfo(1).short_src, {["4"] = 1,["5"] = 3,["6"] = 5,["7"] = 5,["8"] = 5,["9"] = 5,["10"] = 6,["11"] = 7,["13"] = 5,["14"] = 5,["15"] = 11,["16"] = 12});
+__TS__SourceMapTraceBack(debug.getinfo(1).short_src, {["4"] = 1,["5"] = 3,["6"] = 5,["7"] = 5,["8"] = 5,["9"] = 5,["10"] = 6,["11"] = 7,["12"] = 8,["14"] = 5,["15"] = 5});
 centerX, centerY = display.contentCenterX, display.contentCenterY
 player = display.newRect(centerX, centerY, 50, 50)
 Runtime.addEventListener(
@@ -9,6 +9,7 @@ Runtime.addEventListener(
     function(e)
         if e.keyName == "up" then
             player.y = player.y - 5
+            player.x = player.x + 5
         end
     end
 )
--- src/main.ts
+++ src/main.ts
@@ -5,5 +5,6 @@ let player = display.newRect(centerX, centerY, 50, 50)
 Runtime.addEventListener(Runtime, "key", function(e: any){
     if (e.keyName == "up") {
         player.y -= 5
+        player.x += 5
     }
 })
